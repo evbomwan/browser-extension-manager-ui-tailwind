6b588850c6c059e0ee6fc886128773617548a486
evbomwan <evbomwanc@gmail.com>
2025-08-22 12:42:05 +0100

    making progress

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const buttons = document.querySelectorAll(".filter-btn");
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        buttons.forEach(b => b.classList.remove("bg-red-500", "text-white"));
+        btn.classList.add("bg-red-500", "text-white");
+    });
+});
