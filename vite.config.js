b060a3dd958cfa0d1185757b1dd4a7dc190674d8
evbomwan <evbomwanc@gmail.com>
2025-08-22 12:50:42 +0100

    build for Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/browser-extension-manager-ui-tailwind/',
   plugins: [react()],
 })
