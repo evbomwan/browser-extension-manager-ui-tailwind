dc45e8d6dd2ce637d7a75b37b1bb5100af3b8ef5
evbomwan <evbomwanc@gmail.com>
2025-09-04 12:25:13 +0100

    code update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import './style.css'
+let darkmode = false;
+
+document.getElementById('switch').addEventListener('click', () => { 
+    darkmode = !darkmode;
+
+    if (darkmode) {
+        // Dark gradient
+        document.body.style.background = 'linear-gradient(180deg, #040918 0%, #091540 100%)';
+    } else {
+        // Light gradient
+        document.body.style.background = 'linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)';
+    }
+});
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {},
-    },
+    extend: {},
   },
   plugins: [],
 }
