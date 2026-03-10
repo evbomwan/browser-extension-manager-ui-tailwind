7e8e5503ecb3a34f9e1e0ff80a18d5e74b5a5080
evbomwan <evbomwanc@gmail.com>
2026-03-10 18:46:01 +0100

    update code

--- src/main.js
+++ src/main.js
@@ -11,6 +11,27 @@ document.getElementById('switch').addEventListener('click', () => {
         // Light gradient
         document.body.style.background = 'linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)';
     }
+    // button logo
+     const img = document.getElementById('mode');
+     if (darkmode){
+        img.src = './images/icon-sun.svg'
+     }else {
+     img.src = './images/icon-moon.svg'
+    }
+    //extensions list
+    const extension = document.getElementById('extension');
+    if (darkmode) {
+        extension.style.color = 'white';
+    } else {
+        extension.style.color = 'black';
+    }
+    //switch
+    const switchContainer = document.getElementById('switch');
+    if (darkmode){
+        switchContainer.style.background = 'grey';
+    } else{
+         switchContainer.style.background = '';
+    }
 });
 
 
